9aff86f398a2fdf506995d61baacd64755588915
YogaE41181231 <agoyjatim@gmail.com>
2020-10-20 00:04:30 +0700

    Done dashboard user

--- application/views/user/template/v_sidebar.php
+++ application/views/user/template/v_sidebar.php
@@ -1,9 +1,9 @@
     <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-light-dark elevation-4">
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="index3.html" class="brand-link">
-            <img src="<?= base_url(); ?>assets/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-            <span class="brand-text font-weight-light">Admin <b>PA</b></span>
+            <img src="<?= base_url(); ?>assets/dist/img/PA02.jpg" alt="PA Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+            <span class="brand-text font-weight-light">User <b>PA</b></span>
         </a>
 
         <!-- Sidebar -->
