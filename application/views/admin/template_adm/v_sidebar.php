4c56188a307c3aa9f0806b4bac2e33810c413315
Bayu_Agil <54614718+Agilbay04@users.noreply.github.com>
2020-10-23 16:44:53 +0700

    update sidebar

--- application/views/admin/template_adm/v_sidebar.php
+++ application/views/admin/template_adm/v_sidebar.php
@@ -23,6 +23,9 @@
                 <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                     <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
                     <!-- MENU -->
+                    
+                    <!-- ini menu yang ga ada sub menunya -->
+                    <!-- kalo ngopy codingan menunya dari sini -->
                     <li class="nav-item">
                         <a href="<?= base_url('admin/dashboard'); ?>" class="nav-link active">
                             <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -32,6 +35,10 @@
                             </p>
                         </a>
                     </li>
+                    <!-- akhir ngopy sampai sini -->
+
+                    <!-- Ini menu yang ada submenunya -->
+                    <!-- kalo ngopy codingan menunya dari sini -->
                     <li class="nav-item has-treeview">
                         <a href="#" class="nav-link">
                             <i class="nav-icon fas fa-user"></i>
@@ -40,13 +47,17 @@
                                 <i class="right fas fa-angle-left"></i>
                             </p>
                         </a>
+                        <!-- ini submenunya -->
                         <ul class="nav nav-treeview">
+                            <!-- kalo ngopy sub menu dari sini -->
                             <li class="nav-item">
                                 <a href="#" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Profil</p>
                                 </a>
                             </li>
+                            <!-- sampe sini -->
+                            
                             <li class="nav-item">
                                 <a href="#" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
@@ -61,6 +72,8 @@
                             </li>
                         </ul>
                     </li>
+                    <!-- akhir ngopy sampai sini -->
+                
                     <li class="nav-item has-treeview menu-open">
                         <a href="#" class="nav-link">
                             <i class="nav-icon fas fa-blog"></i>
