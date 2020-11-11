9c5d6bc5cee1224266ff1f8b98ab4e75138b993d
Muhammad Arif Annaili Fitrawan-E41181232 <arlopaz.uye121299@gmail.com>
2020-11-12 01:45:56 +0700

    Setting website 1.5 (Medsos Done)

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -6,6 +6,7 @@ class index extends CI_Controller
     {
         parent::__construct();
         $this->load->model('m_landingpage');
+        $this->load->model('admin/m_medsos');
     }
 
 
@@ -13,10 +14,11 @@ class index extends CI_Controller
     public function index()
     {
         $data['blog'] = $this->m_landingpage->tampil_blog_web()->result();
+        $data['data'] = $this->m_medsos->get_data(); 
         $data['judul'] = 'Preneur Academy';
         $this->load->view("landingpage/template/header", $data);
         $this->load->view("landingpage/index");
-        $this->load->view("landingpage/template/footer");
+        $this->load->view("landingpage/template/footer" , $data);
     }
 
     public function detail_blog()
