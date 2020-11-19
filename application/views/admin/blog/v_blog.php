e4921aa32f4f95a819d9e01d313a64a68042d28b
mustikakhoiri <mustikakhoiri15@gmail.com>
2020-11-20 00:24:25 +0700

    berhenti eksperimen v_blog

--- application/views/admin/blog/v_blog.php
+++ application/views/admin/blog/v_blog.php
@@ -29,99 +29,92 @@
 			</div>
 		</div>
 		<?php foreach ($blog as $blg) { ?>
-		<div class="row">
-			<div class="col-12">
-				<div class="card">
-					<div class="card-body">
-						<div class="tab-content">
-							<div class="active tab-pane" id="activity">
-								<!-- Post -->
-								<div class="post">
-									<div class="user-block">
-										<!-- <img class="img-circle img-bordered-sm"
-											src="<?= base_url('assets/fotoicon/'. $blg->FOTO_POST); ?>"> -->
-										<span class="username m-0 text-lg">
-											<a class="text-dark"
-												href="<?= base_url('admin/blog/edit_artikel/' . $blg->ID_POST.'/'); ?>"><?= $blg->JUDUL_POST; ?></a>
+			<div class="row">
+				<div class="col-12">
+					<div class="card">
+						<div class="card-body">
+							<div class="tab-content">
+								<div class="active tab-pane" id="activity">
+									<!-- Post -->
+									<div class="post">
+										<div class="user-block">
+											<!-- <img class="img-circle img-bordered-sm"
+											src="<?= base_url('assets/fotoicon/' . $blg->FOTO_POST); ?>"> -->
+											<span class="username m-0 text-lg">
+												<a class="text-dark" href="<?= base_url('admin/blog/edit_artikel/' . $blg->ID_POST . '/'); ?>"><?= $blg->JUDUL_POST; ?></a>
 
-										</span>
-										<hr>
-										<span>
-											<i class="fa fa-folder"></i>
-											<a class="link-black text-lg"
-												href="<?= base_url('admin/blog/lihat_post_ktg'); ?>"><?= $blg->NM_CT; ?></a>
+											</span>
+											<hr>
+											<span>
+												<i class="fa fa-folder"></i>
+												<a class="link-black text-lg" href="<?= base_url('admin/blog/lihat_post_ktg'); ?>"><?= $blg->NM_CT; ?></a>
 
-										</span>
-									</div>
-									<!-- /.user-block -->
-									<!-- Karepnya nampilin sebagian kalimat di artikel -->
-									<p>
-										<?php 
-										$i = 100;
-										$KONTEN_POST = htmlspecialchars_decode($blg->KONTEN_POST);
-										$konten = htmlspecialchars_decode(substr($KONTEN_POST, 0, $i));
+											</span>
+										</div>
+										<!-- /.user-block -->
+										<!-- Karepnya nampilin sebagian kalimat di artikel -->
+										<p>
+											<!-- <?php
+													$i = 100;
+													$KONTEN_POST = htmlspecialchars_decode($blg->KONTEN_POST);
+													$konten = htmlspecialchars_decode(substr($KONTEN_POST, 0, $i));
 
-										$char = $KONTEN_POST[$i - 1];
-										while($char != ' ') {
-											$char = $KONTEN_POST[--$i]; // Cari spasi pada posisi 49, 48, 47, dst...
-										}
-										echo substr($KONTEN_POST, 0, $i) . ' ...';
-										?>
-									</p>
+													$char = $KONTEN_POST[$i - 1];
+													while ($char != ' ') {
+														$char = $KONTEN_POST[--$i]; // Cari spasi pada posisi 49, 48, 47, dst...
+													}
+													echo substr($KONTEN_POST, 0, $i) . ' ...';
+													?> -->
+										</p>
 
-									<p>
-										<!-- Nyari status post trus ditampilkan sesuai status post -->
-										<?php 
-										if ($blg->ST_POST == 0) {
-											echo '<label for="">Draf</label>';
-										} else {
-											echo '<label for="">Dipublikasikan</label>';
-										}
-										?>
-
-										<label for="TGL_POST"
-											class="text-sm mr-2"><?= ' | '. date('d F Y', strtotime($blg->TGL_POST)); ?></label>
-										<span class="float-right">
-											<!-- Nyari status post trus mau diposting apa nggak -->
-											<?php 
+										<p>
+											<!-- Nyari status post trus ditampilkan sesuai status post -->
+											<?php
 											if ($blg->ST_POST == 0) {
-												echo '<button type="button" id="detail" class="btn btn-warning btn-sm btn-round" style="color: white"
-												data-toggle="modal" data-target="#modal_posting'. $blg->ID_POST.'">
-												<i class="fas fa-arrow-circle-right"></i> Publikasikan</button>';
+												echo '<label for="">Draf</label>';
 											} else {
-												echo '<button type="button" id="detail" class="btn btn-success btn-sm btn-round" style="color: white"
-												data-toggle="modal" data-target="#modal_posting'. $blg->ID_POST.'">
-												<i class="fas fa-arrow-circle-left"></i> Kembalikan ke draf</button>';	
+												echo '<label for="">Dipublikasikan</label>';
 											}
 											?>
-											<!-- dilihat tampilan blognya sebelum diposting -->
-											<a class="btn btn-secondary btn-sm btn-round"
-												href="<?= base_url('admin/blog/pratinjau/'.$blg->ID_POST);?>"><i
-													class="fas fa-eye"></i> Pratinjau</a>
 
-											<!-- edit artikel -->
-											<a href="<?= base_url('admin/blog/edit_artikel/' .$blg->ID_POST); ?>">
-												<button type="button" class="btn btn-primary btn-circle btn-sm">
-													<i class="fas fa-edit" style="color: white"></i> Edit
-												</button>
-											</a>
-											<!-- hapus artikel -->
-											<button type="button" id="detail" class="btn btn-danger btn-circle btn-sm"
-												data-toggle="modal" data-target="#modal_hapus<?= $blg->ID_POST; ?>"
-												style="color : white"><i class="fas fa-trash"></i> Hapus</button>
-										</span>
-									</p>
+											<label for="TGL_POST" class="text-sm mr-2"><?= ' | ' . date('d F Y', strtotime($blg->TGL_POST)); ?></label>
+											<span class="float-right">
+												<!-- Nyari status post trus mau diposting apa nggak -->
+												<?php
+												if ($blg->ST_POST == 0) {
+													echo '<button type="button" id="detail" class="btn btn-warning btn-sm btn-round" style="color: white"
+												data-toggle="modal" data-target="#modal_posting' . $blg->ID_POST . '">
+												<i class="fas fa-arrow-circle-right"></i> Publikasikan</button>';
+												} else {
+													echo '<button type="button" id="detail" class="btn btn-success btn-sm btn-round" style="color: white"
+												data-toggle="modal" data-target="#modal_posting' . $blg->ID_POST . '">
+												<i class="fas fa-arrow-circle-left"></i> Kembalikan ke draf</button>';
+												}
+												?>
+												<!-- dilihat tampilan blognya sebelum diposting -->
+												<a class="btn btn-secondary btn-sm btn-round" href="<?= base_url('admin/blog/pratinjau/' . $blg->ID_POST); ?>"><i class="fas fa-eye"></i> Pratinjau</a>
+
+												<!-- edit artikel -->
+												<a href="<?= base_url('admin/blog/edit_artikel/' . $blg->ID_POST); ?>">
+													<button type="button" class="btn btn-primary btn-circle btn-sm">
+														<i class="fas fa-edit" style="color: white"></i> Edit
+													</button>
+												</a>
+												<!-- hapus artikel -->
+												<button type="button" id="detail" class="btn btn-danger btn-circle btn-sm" data-toggle="modal" data-target="#modal_hapus<?= $blg->ID_POST; ?>" style="color : white"><i class="fas fa-trash"></i> Hapus</button>
+											</span>
+										</p>
+									</div>
+									<!-- /.post -->
 								</div>
-								<!-- /.post -->
+								<!-- /.tab-pane -->
 							</div>
-							<!-- /.tab-pane -->
+							<!-- /.tab-content -->
 						</div>
-						<!-- /.tab-content -->
+						<!-- /.card-body -->
 					</div>
-					<!-- /.card-body -->
 				</div>
 			</div>
-		</div>
 		<?php } ?>
 	</section>
 	<!-- /.content -->
@@ -129,69 +122,69 @@
 <!-- /.content-wrapper -->
 
 <!-- modal posting -->
-<?php 
-foreach ($blog as $blg) { 
+<?php
+foreach ($blog as $blg) {
 	$ID_POST = $blg->ID_POST;
-	$ST_POST = $blg->ST_POST;	
-	?>
-<div class="modal fade" id="modal_posting<?= $ID_POST; ?>" tabindex="-1" role="dialog" aria-hidden="true">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<?php 
-			if ($ST_POST == 0) {
-				echo '<div class="modal-header">
+	$ST_POST = $blg->ST_POST;
+?>
+	<div class="modal fade" id="modal_posting<?= $ID_POST; ?>" tabindex="-1" role="dialog" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<?php
+				if ($ST_POST == 0) {
+					echo '<div class="modal-header">
 						<h3 class="modal-title" id="myModalLabel">Publikasikan Artikel</h3>
 					</div>
-					<form action="'. base_url('admin/blog/pr_posting').'" method="post" class="form-horizontal">
+					<form action="' . base_url('admin/blog/pr_posting') . '" method="post" class="form-horizontal">
 						<div class="modal-body">
 							<p>Apakah Anda yakin ingin mempublikasikan artikel ini?</p>
 						</div>
 						<div class="modal-footer">
-							<input type="hidden" name="ST_POST" value="'. $ST_POST. '">
-							<input type="hidden" name="ID_POST" value="'. $ID_POST. '">
+							<input type="hidden" name="ST_POST" value="' . $ST_POST . '">
+							<input type="hidden" name="ID_POST" value="' . $ID_POST . '">
 							<button class="btn btn-secondary" data-dismiss="modal" aria-hidden="true">Batal</button>
 							<button class="btn btn-primary">Ya</button>
 						</div>
 					</form>';
-			} else {
-				echo '<div class="modal-header">
+				} else {
+					echo '<div class="modal-header">
 						<h3 class="modal-title" id="myModalLabel">Kembalikan ke Draf</h3>
 					</div>
-					<form action="'. base_url('admin/blog/pr_posting').'" method="post" class="form-horizontal">
+					<form action="' . base_url('admin/blog/pr_posting') . '" method="post" class="form-horizontal">
 						<div class="modal-body">
 							<p>Apakah Anda ingin mengembalikan artikel ke draf?</p>
 						</div>
 						<div class="modal-footer">
-							<input type="hidden" name="ST_POST" value="'. $ST_POST. '">
-							<input type="hidden" name="ID_POST" value="'. $ID_POST. '">
+							<input type="hidden" name="ST_POST" value="' . $ST_POST . '">
+							<input type="hidden" name="ID_POST" value="' . $ID_POST . '">
 							<button class="btn btn-secondary" data-dismiss="modal" aria-hidden="true">Batal</button>
 							<button class="btn btn-primary">Ya</button>
 						</div>
 					</form>';
-			}
-			?>
-		</div>
-	</div>
-</div>
-
-<!-- Modal Hapus -->
-<div class="modal fade" id="modal_hapus<?= $ID_POST; ?>" tabindex="-1" role="dialog" aria-hidden="true">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h3 class="modal-title" id="myModalLabel">Hapus Data</h3>
+				}
+				?>
 			</div>
-			<form action="<?= base_url('admin/blog/hapus_artikel'); ?>" method="post" class="form-horizontal">
-				<div class="modal-body">
-					<p>Apakah Anda yakin ingin menghapus data ini?</p>
-				</div>
-				<div class="modal-footer">
-					<input type="hidden" name="ID_POST" value="<?= $ID_POST; ?>">
-					<button class="btn btn-secondary" data-dismiss="modal" aria-hidden="true">Batal</button>
-					<button class="btn btn-danger">Hapus</button>
-				</div>
-			</form>
 		</div>
 	</div>
-</div>
+
+	<!-- Modal Hapus -->
+	<div class="modal fade" id="modal_hapus<?= $ID_POST; ?>" tabindex="-1" role="dialog" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h3 class="modal-title" id="myModalLabel">Hapus Data</h3>
+				</div>
+				<form action="<?= base_url('admin/blog/hapus_artikel'); ?>" method="post" class="form-horizontal">
+					<div class="modal-body">
+						<p>Apakah Anda yakin ingin menghapus data ini?</p>
+					</div>
+					<div class="modal-footer">
+						<input type="hidden" name="ID_POST" value="<?= $ID_POST; ?>">
+						<button class="btn btn-secondary" data-dismiss="modal" aria-hidden="true">Batal</button>
+						<button class="btn btn-danger">Hapus</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
 <?php } ?>
