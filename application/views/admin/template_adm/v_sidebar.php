4f0f0c6a12075efa32fd9e1195fcb469270d7e89
ansyahdotcom <55609786+ansyahdotcom@users.noreply.github.com>
2020-11-27 18:04:53 +0700

    ubah sidebar

--- application/views/admin/template_adm/v_sidebar.php
+++ application/views/admin/template_adm/v_sidebar.php
@@ -3,7 +3,7 @@
         <!-- Brand Logo -->
         <a href="index3.html" class="brand-link">
             <img src="<?= base_url(); ?>assets/dist/img/PA02.jpg" alt="PA Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-            <span class="brand-text font-weight-bold text-uppercase"><b> Preneur Academy </b></span>
+            <span class="brand-text font-weight-bold text-uppercase"><h5> Preneur Academy </h5></span>
         </a>
 
         <!-- Sidebar -->
@@ -199,12 +199,12 @@
                                     <p>Kategori Kelas</p>
                                 </a>
                             </li>
-                            <li class="nav-item">
+                            <!-- <li class="nav-item">
                                 <a href="<?= base_url('admin/diskon'); ?>" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Data Diskon</p>
                                 </a>
-                            </li>
+                            </li> -->
                             <li class="nav-item">
                                 <a href="<?= base_url('admin/#'); ?>" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
