01705c84af2adc64f175751e50cd4ce391754653
ansyahdotcom <55609786+ansyahdotcom@users.noreply.github.com>
2020-12-12 17:50:03 +0700

    Update dt_kls.php

--- application/views/landingpage/dt_kls.php
+++ application/views/landingpage/dt_kls.php
@@ -18,7 +18,7 @@
 		</div>
 		
 		<div class="row justify-content-center">
-			<div class="col-md-9">
+			<div class="col-md-6">
 				<h3 class="my-3">Deskripsi Kelas</h3>
 				<p><?= $row->DESKRIPSI;?></p>
 				<h3 class="my-3">Detail Kelas</h3>
