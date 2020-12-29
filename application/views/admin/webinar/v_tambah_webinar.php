8114178d1c2468e8446002dd15a956d6dcbdbb5a
mustikakhoiri <mustikakhoiri15@gmail.com>
2020-12-29 23:27:55 +0700

    sertifikat

--- application/views/admin/webinar/v_tambah_webinar.php
+++ application/views/admin/webinar/v_tambah_webinar.php
@@ -5,13 +5,13 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">Tambah Webinar</h1>
+                    <h1 class="m-0 text-dark"><?= $tittle ?></h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="<?= base_url('admin/dashboard'); ?>">Home</a></li>
                         <li class="breadcrumb-item"><a href="<?= base_url('admin/webinar'); ?>">Webinar</a></li>
-                        <li class="breadcrumb-item active">Tambah Webinar</li>
+                        <li class="breadcrumb-item active"><?= $tittle ?></li>
                     </ol>
                 </div><!-- /.col -->
             </div><!-- /.row -->
