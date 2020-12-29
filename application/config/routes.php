7ed4864d85fdcac216cf4e2f134f5c92c7ecef21
Muhammad Arif Annaili Fitrawan-E41181232 <arlopaz.uye121299@gmail.com>
2020-12-29 13:47:18 +0700

    blocked page

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,6 +52,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $route['default_controller'] = 'index';
 // $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
+$route['block'] = 'index/block';
 $route['translate_uri_dashes'] = FALSE;
 //$route['admin'] = 'admin/overview';
 
